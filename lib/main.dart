92fb54c13bbadedf84d4353870fd7e0e1e7efe65
Alexandros Spyridis <alexspy1@gmail.com>
2019-11-17 21:03:58 +0200

    Production Updates

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter UI Collections',
+      title: 'Endevour',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: themeColour,
